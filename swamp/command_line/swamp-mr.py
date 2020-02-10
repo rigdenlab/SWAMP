0004a9cc0de9751d44d3869e76f00a2ba9c2884a
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-02-10 22:53:23 +0000

    solve merge conflicts, closes #8

--- swamp/command_line/swamp-mr.py
+++ swamp/command_line/swamp-mr.py
@@ -7,7 +7,7 @@ import traceback
 import itertools
 from swamp.mr.core.mrjob import MrJob
 from swamp.mr.core.mrarray import MrArray
-from swamp.mr.mrresults import MrResults
+from swamp.mr.core.mrresults import MrResults
 from swamp.logger.swamplogger import SwampLogger
 from swamp.command_line import check_file_exists
 from swamp.library.scan.scantarget import ScanTarget
