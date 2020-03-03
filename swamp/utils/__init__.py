545c895586bdcc5f888677c17ba3dc934226fe7a
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-03-03 10:38:06 +0000

    fixed! compress files works now with py2

--- swamp/utils/__init__.py
+++ swamp/utils/__init__.py
@@ -7,6 +7,7 @@ __author__ = "Filomeno Sanchez Rodriguez"
 __credits__ = "Daniel Rigden, & Ronan Keegan"
 __email__ = "filomeno.sanchez-rodriguez@liv.ac.uk"
 
+import sys
 import os
 import gzip
 import shutil
@@ -61,7 +62,10 @@ def compress(fname, out=None):
 
     with open(fname, 'rb') as f_in, gzip.open(out, 'wb') as f_out:
         data = f_in.read()
-        bindata = bytearray(data)
+        if sys.version_info[0] < 3:
+            bindata = data
+        else:
+            bindata = bytearray(data)
         f_out.write(bindata)
 
     return out
